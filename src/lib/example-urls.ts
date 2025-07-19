f833b150ec739e735af275a22e60579b2a125d9d
おんJ民 <J onjmin@users.noreply.github.com>
2025-07-19 13:00:52 +0900

    fix

--- src/lib/example-urls.ts
+++ src/lib/example-urls.ts
@@ -1,7 +1,10 @@
 export const exampleUrls = `
-https://www.youtube.com/watch?v=v1UwWciLNMo
 https://www.youtube.com/watch?v=MByMCC57E3g
 https://www.youtube.com/watch?v=3g-gzibOR-A
+https://www.youtube.com/watch?v=-uYSArEQKGA
 https://www.nicovideo.jp/watch/sm2312089
-https://soundcloud.com/myukke/beta-snow-for-badly-wood-cup-2024
+https://www.nicovideo.jp/watch/sm21700328
+https://soundcloud.com/sound-sepher/vigddyavn3ax
+https://soundcloud.com/beatmario/cccd0035-sample
+https://soundcloud.com/mk240le7xjr8/merge-funky-roadbpm198
 `.trim();
